108bbbe907e713d2f2719df142140bc5892cd98b
ffontinele <seu_email@exemplo.com>
2026-09-05 13:47:11 -0400

    CI: adiciona GitHub Actions para compilar APK automaticamente em x86_64

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zui.iptv.android',
+  appName: 'ZUI IPTV',
+  webDir: 'dist'
+};
+
+export default config;
